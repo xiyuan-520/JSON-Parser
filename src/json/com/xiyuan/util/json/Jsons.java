77792ca838cc8e120ea04536e20139e156e687ea
lgz <lgz@zhiqim.com>
2019-09-27 10:18:28 +0800

    微调

--- src/json/com/xiyuan/util/json/Jsons.java
+++ src/json/com/xiyuan/util/json/Jsons.java
@@ -13,7 +13,7 @@ import java.util.List;
 public final class Jsons implements Serializable
 {
     private static final long serialVersionUID = 1L;
-
+    
     /**
      * 增加JSON中的转义字符，使用双引号时，单引号不转义，使用单引号时双引号不转义，不使用引号时都转义
      * 
@@ -25,7 +25,7 @@ public final class Jsons implements Serializable
     {
         return JsonLexer.addEscapeChar(str, quotation);
     }
-
+    
     /***
      * 去除JSON中的转义字符
      * 
@@ -36,7 +36,7 @@ public final class Jsons implements Serializable
     {
         return JsonLexer.removeEscapeChar(str);
     }
-
+    
     /***
      * 去除JSON键和值的前后成对引号
      * 
@@ -47,11 +47,11 @@ public final class Jsons implements Serializable
     {
         return JsonLexer.removeStartEndQuotation(str);
     }
-
+    
     /*****************************************************************************/
     // 以下是json解析或转json相关方法
     /*****************************************************************************/
-
+    
     /**
      * 对象转换成JSON字符串
      * 
@@ -64,7 +64,7 @@ public final class Jsons implements Serializable
             return JsonLexer.NULL;
         return new JsonLexer(null).getParser(obj.getClass()).toString(obj);
     }
-
+    
     /**
      * JSON字符串转换成对象
      * 
@@ -77,17 +77,17 @@ public final class Jsons implements Serializable
     {
         if (json == null || clazz == null)
             return null;
-
+        
         if (JsonLexer.NULL.equals(json))
             return null;
-
+        
         JsonLexer lexer = new JsonLexer(json);
         lexer.naxtToken();
         T t = (T) lexer.getParser(clazz).toObject(clazz);
         lexer = null;
         return t;
     }
-
+    
     /**
      * 提供泛型，得到列表
      * 
@@ -110,25 +110,25 @@ public final class Jsons implements Serializable
             else
                 return new ArrayList<T>(0);// 第一个token不是数组
         }
-
+        
         if (!isList)
             return new ArrayList<T>(0);
-
+        
         if (JsonLexer.isPrimitiveBase(resultClass))// 如果是基本类型 则使用基本类型的包装类
             resultClass = JsonLexer.getPrimitiveBase(resultClass);
         // 先转成数组再转list
         T[] arr = (T[]) lexer.ArrayParser().toObject(Array.newInstance(resultClass, 0).getClass());
         if (arr == null)
             return new ArrayList<T>(0);
-
+        
         List<T> ls = new ArrayList<T>(arr.length);
         for (int i = 0; i < arr.length; i++)
             ls.add(arr[i]);
-
+        
         lexer = null;
         return ls;
     }
-
+    
     /**
      * 获取字段对应的数组字符串值，得到结果为[]开头和结束
      * 
@@ -145,7 +145,7 @@ public final class Jsons implements Serializable
             return (T[]) Array.newInstance(resultClass, 0);
         return (T[]) arr;
     }
-
+    
     /**
      * 获取boolean 如果 值为1，结果也返回true
      */
@@ -153,7 +153,7 @@ public final class Jsons implements Serializable
     {
         return getObject(json, key, boolean.class);
     }
-
+    
     /**
      * 指定字段获取int值，若没有对应值则返回0
      */
@@ -161,7 +161,7 @@ public final class Jsons implements Serializable
     {
         return getObject(json, key, int.class);
     }
-
+    
     /***
      * 指定json字段获取一个long值
      * 
@@ -173,7 +173,7 @@ public final class Jsons implements Serializable
     {
         return getObject(json, key, long.class);
     }
-
+    
     /**
      * 从json字符串中删除字段 当前只删除 一级字段<br>
      * 
@@ -183,7 +183,7 @@ public final class Jsons implements Serializable
      */
     public static String remove(String json, String key)
     {
-
+        
         int start = 0;
         boolean isValue = false, findKey = false;
         String k = null;
@@ -194,7 +194,7 @@ public final class Jsons implements Serializable
             lexer.naxtToken();
             if (!findKey && !lexer.isString())
                 continue;// 不是String 型的key
-
+                
             if (lexer.isString() && !findKey && key.equals(JsonLexer.removeStartEndQuotation(lexer.value())))
             {
                 findKey = true;// 找到key
@@ -203,32 +203,32 @@ public final class Jsons implements Serializable
                 scope = lexer.scope();
                 continue;
             }
-
+            
             if (!isValue && findKey && !lexer.isColon())
             {// 找到key 但是下一个token不是 冒号 & 没有标记value开始
                 findKey = false;
                 continue;
             }
-
+            
             if (!isValue && lexer.isColon())
             {// 没有标记开始 但是当前是冒号表示 下一个是值
                 isValue = true;
                 continue;
             }
-
+            
             if (isValue && findKey)
             { // start是最后的引号值
-
+            
                 lexer.BaseParser().toObject(String.class);// 设置value值得结束位置
                 if (lexer.hasNext())
                     lexer.naxtToken();
-
+                
                 int end = lexer.pos();// 设置value结束位置,当前位置可能是 子object的结束符
                 if (lexer.scope() == scope)
                     end++;// 相同深度的与key 相同深度value 时 指向下一个token
-
+                    
                 start = start + 1 - k.length();
-
+                
                 if (end >= json.length())
                     return json.substring(0, start);
                 else
@@ -241,10 +241,10 @@ public final class Jsons implements Serializable
                 }
             }
         }
-
+        
         return json;
     }
-
+    
     /**
      * 从一个对象或MAP的json中增加或更新指定的一个KEY<br>
      * 注意：<br>
@@ -266,37 +266,37 @@ public final class Jsons implements Serializable
             lexer.naxtToken();
             if (!findKey && !lexer.isString())
                 continue;// 不是String 型的key
-
+                
             if (lexer.isString() && !findKey && key.equals(JsonLexer.removeStartEndQuotation(lexer.value())))
             {
                 findKey = true;// 找到key
                 scope = lexer.scope();
                 continue;
             }
-
+            
             if (!isValue && findKey && !lexer.isColon())
             {// 找到key 但是下一个token不是 冒号 & 没有标记value开始
                 findKey = false;
                 continue;
             }
-
+            
             if (!isValue && lexer.isColon())
             {// 没有标记开始 但是当前是冒号表示 下一个是值
                 isValue = true;
                 start = lexer.pos() + 1;
                 continue;
             }
-
+            
             if (isValue && findKey)
             { // start是最后的引号值
                 lexer.BaseParser().toObject(String.class);// 设置value值得结束位置
                 if (lexer.hasNext())
                     lexer.naxtToken();
-
+                
                 int end = lexer.pos();// 设置value结束位置,当前位置可能是 子object的结束符
                 if (lexer.scope() == scope)
                     end++;// 相同深度的与key 相同深度value 时 指向下一个token
-
+                    
                 String val = value == null ? JsonLexer.NULL : lexer.getParser(value.getClass()).toString(value);
                 String startStr = json.substring(0, start);
                 if (end >= json.length())
@@ -305,7 +305,7 @@ public final class Jsons implements Serializable
                     return startStr + val + JsonLexer.trim(json.substring(end));
             }
         }
-
+        
         // 找不到字段则插入
         boolean findScope = false;
         lexer = new JsonLexer(json);
@@ -318,7 +318,7 @@ public final class Jsons implements Serializable
                 break;
             }
         }
-
+        
         if (findScope)
         {
             key = lexer.BaseParser().toString(key);
@@ -327,16 +327,16 @@ public final class Jsons implements Serializable
             sb.append(key).append(JsonLexer.COLON).append(val);// 新增值
             if (lexer.pos() + 1 < json.length())
             {
-                if ((json.charAt(lexer.pos() + 1) != JsonLexer.BRACE_R  && (json.charAt(lexer.pos() + 1) != JsonLexer.BRACKET_R)))
+                if ((json.charAt(lexer.pos() + 1) != JsonLexer.BRACE_R && (json.charAt(lexer.pos() + 1) != JsonLexer.BRACKET_R)))
                     sb.append(JsonLexer.COMMA);
                 sb.append(json.substring(lexer.pos() + 1));// 添加剩余值
             }
             return sb.toString();
         }
-
+        
         return json;
     }
-
+    
     /**
      * 提供两个键值生成JSON字符串，对应MAP的两次put
      * 
@@ -348,10 +348,10 @@ public final class Jsons implements Serializable
     {
         if (isEmptyBlank(key))
             return JsonLexer.NULL;
-
+        
         return new StringBuilder().append(JsonLexer.BRACE_L).append(toString(trim(key))).append(JsonLexer.COLON).append(toString(value)).append(JsonLexer.BRACE_R).toString();
     }
-
+    
     /**
      * 指定字段查找json字符串的数据
      * 
@@ -363,7 +363,7 @@ public final class Jsons implements Serializable
     {//
         return getObject(json, key, String.class);
     }
-
+    
     /***
      * 指定字段查找及对应的类型
      * @param json json 字符串
@@ -376,39 +376,45 @@ public final class Jsons implements Serializable
     {//
         if (json == null || isEmptyBlank(key))
             return null;
-
+        int ind = json.indexOf(key);
+        if (ind == -1)
+            return null;
+        
         JsonLexer lexer = new JsonLexer(json);
         boolean isValue = false, findKey = false;
         while (lexer.hasNext())
         {
             lexer.naxtToken();
+            if (lexer.pos() < ind)
+                continue;// 未找到到到指定索引
+                
             if (!findKey && !lexer.isString())
                 continue;// 不是String 型的key
-
+                
             if (lexer.isString() && !findKey && key.equals(JsonLexer.removeStartEndQuotation(lexer.value())))
             {
                 findKey = true;// 找到key
                 continue;
             }
-
-            if (!isValue && findKey && !lexer.isColon())
+            
+            if (findKey && !isValue && !lexer.isColon())
             {// 找到key 但是下一个token不是 冒号 & 没有标记value开始
                 findKey = false;
                 continue;
             }
-
+            
             if (!isValue && lexer.isColon())
             {// 没有标记开始 但是当前是冒号表示 下一个是值
                 isValue = true;
                 continue;
             }
-
+            
             if (isValue)
                 return (T) lexer.getParser(cls).toObject(cls);
         }
         return null;
     }
-
+    
     /**************************************************************************/
     // 以下提供验证方法
     /**************************************************************************/
@@ -422,7 +428,7 @@ public final class Jsons implements Serializable
     {// 中文空格 =12288=0x3000, BOM空格 =65279=0xFEFF，英文空格 = 32
         return ch <= 32 || ch == 12288 || ch == 65279;
     }
-
+    
     /**
      * 判断类结构，是否实现指定的接口（含本类）
      * 
@@ -434,10 +440,10 @@ public final class Jsons implements Serializable
     {
         if (iface == null || cls == null || cls == Object.class || isPrimitiveBase(cls) || !iface.isInterface())
             return false;
-
+        
         if (cls == iface)
             return true;
-
+        
         // 先判断接口是否实现，或实现的接口是否继承
         for (Class<?> c : cls.getInterfaces())
         {
@@ -446,28 +452,28 @@ public final class Jsons implements Serializable
                 Class<?>[] cs = c.getInterfaces();
                 if (cs.length == 0)
                     break;
-
+                
                 c = cs[0];
             }
-
+            
             // 最后判断是否实现
             if (c == iface)
                 return true;
         }
-
+        
         // 再判断继承是否实现接口，有可能cls本身是接口
         Class<?> c = cls.getSuperclass();
         while (c != null && c != Object.class)
         {
             if (isImplement(c, iface))
                 return true;
-
+            
             c = c.getSuperclass();
         }
-
+        
         return false;
     }
-
+    
     /**
      * 是否八种基本原型基本类型，仅支持byte/short/int/long/float/double/boolean/char
      * 
@@ -478,7 +484,7 @@ public final class Jsons implements Serializable
     {
         return JsonLexer.isPrimitiveBase(clazz);
     }
-
+    
     /**
      * 判断是否为空
      * 
@@ -489,12 +495,12 @@ public final class Jsons implements Serializable
     {
         return (s == null || s.length() == 0);
     }
-
+    
     public static boolean isEmptyBlank(String s)
     {
         return JsonLexer.isEmptyBlank(s);
     }
-
+    
     public static String trim(String s)
     {
         return JsonLexer.trim(s);
